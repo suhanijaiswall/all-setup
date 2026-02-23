7915fd1e5693d69939cd6abfc39bdc28439039c0
suhanijaiswal2422 <suhanijaiswal2422@gmail.com>
2026-02-23 13:20:20 +0530

    sh-file

---
+++ jenkins.sh
@@ -0,0 +1,9 @@
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/rpm-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+# Add required dependencies for the jenkins package
+sudo yum install java-17-amazon-corretto jenkins -y
+sudo yum install jenkins
+sudo systemctl restart jenkins
+sudo systemctl daemon-reload
